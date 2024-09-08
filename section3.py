efd567e48f511c2aa0b653eed7b3859cac2156e3
Alishh <19amuhammad@keshacademy.com>
2024-09-08 19:14:36 +0100

    Create section3.py

---
+++ section3.py
@@ -0,0 +1,31 @@
+#section 3 task 3
+age = int(input( "Enter your age: "))
+print ("you will turn 70 in", 70 - age, "years")
+
+
+
+#python task 3b
+one = int(input("Enter the first number: "))
+two = int(input("Enter the second number: "))
+print(one, "+", two, "=", one + two)
+print(one, "-", two, "=", one - two)
+print(one, "*", two, "=", one * two)
+print(one, "/", two, "=", one / two)
+
+
+
+#python 3a task 1
+hour = int(input("What is the hour? "))
+min = int(input("What is the minute? "))
+print (f"The time is {hour}:{min}")
+
+#other tasks
+total = 45
+
+print("You owe £" + str(total) , "in total.")
+
+colour = input("""whats your fav colour? 
+""")
+food = input("""whats your fav food? 
+""")
+print("your favourite colour is", colour, "and your favourite food is", food)
